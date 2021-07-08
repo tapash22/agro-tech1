e8b0d57adad3b68499cada16c909b78aa7cdb552
tapash22 <tapasp263@gmail.com>
2021-07-09 02:46:54 +0600

    scroll response

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -26,6 +26,14 @@
   </div>
 </template>
 
+<script>
+export default {
+    mounted() {
+    window.scrollTo(0, 0);
+  },
+}
+</script>
+
 <style scoped>
 
 .row {
--- src/components/pages/Partners.vue
+++ src/components/pages/Partners.vue
@@ -114,6 +114,9 @@ export default {
       ],
     };
   },
+    mounted() {
+    window.scrollTo(0, 0);
+  },
 };
 </script>
 

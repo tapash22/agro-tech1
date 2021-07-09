07762ad5bafcb223ea68523904d55b8eaa04bbba
tapash22 <31130310+tapash22@users.noreply.github.com>
2021-07-09 14:26:13 +0600

    Update Details.vue

--- src/services/Details.vue
+++ src/services/Details.vue
@@ -1,40 +1,38 @@
 <template>
-  <div class="row">
     <div class="text">
       <p>"Quality is foremost vigor"</p>
     </div>
-  </div>
 </template>
 
 <style scoped>
-.row {
+.text {
   position: fixed;
   right: 0;
-  width: 700px;
+  width: 50%;
   margin-top: -15px;
   height: 35px;
-  background:green;
   margin-left: 2px;
   z-index: 1;
-  border-bottom-left-radius: 90%;
-}
-.text {
-  position: relative;
-  overflow: hidden;
-  margin-left: 25%;
-  padding: 5px;
+   background: linear-gradient(
+    to right,
+    #fff 0%,
+    #fff 50%,
+    #08ce33 50%,
+    #1eda0d 100%
+  );
 }
 .text p {
   font-size: 1.2em;
   font-weight: 500;
-  color: #fff;
+  color: #000;
   position: absolute;
-  width: 100%;
+  width: 50%;
   height: 100%;
   margin: 0;
+  right: 0;
   text-align: center;
   transform: translateX(100%);
-  animation: text 10s linear infinite;
+  animation: text 9s linear infinite;
 }
 
 @keyframes text {
@@ -45,13 +43,5 @@
     transform: translateX(-100%);
   }
 }
-@media only screen and (max-width: 767px) {
-  .row {
-    position: fixed;
-    width: 600px;
-    height: 35px;
-    background: rgb(3, 77, 7);
-    z-index: 1;
-  }
-}
+
 </style>
